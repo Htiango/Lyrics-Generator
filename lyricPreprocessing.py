1f542b6d50c085e45f61e9abc954c2b5e50d083e
Htiango <hongty106@gmail.com>
2018-04-13 23:09:35 -0400

    add processing script for later using

---
+++ lyricPreprocessing.py
@@ -0,0 +1,129 @@
+import nltk
+from collections import Counter
+import numpy as np
+
+START_MARK = "["
+END_MARK = "]"
+
+def seperate(docs_ls):
+    docs_raw = [tokenize(doc) for doc in docs_ls]
+    docs = remove_stopwords(docs_raw)
+    add_mark(docs)
+    return docs
+
+def remove_stopwords(docs):
+    stopwords=nltk.corpus.stopwords.words('english')
+    stopwords = tokenize(' '.join(stopwords))
+    stopwords.extend(get_rare_words(docs))
+    stopwords = set(stopwords)
+    res = [[word for word in doc if word not in stopwords ] for doc in docs]
+    return res
+
+def add_mark(docs):
+	for doc in docs:
+		doc.append(END_MARK)
+		doc.insert(0, START_MARK)
+
+def tokenize(text, lemmatizer=nltk.stem.wordnet.WordNetLemmatizer()):
+    """ Normalizes case and handles punctuation
+    Inputs:
+        text: str: raw text
+        lemmatizer: an instance of a class implementing the lemmatize() method
+                    (the default argument is of type nltk.stem.wordnet.WordNetLemmatizer)
+    Outputs:
+        list(str): tokenized text
+    """
+    text = text.lower()
+    text = text.replace("'s", '')
+    text = text.replace("'", '')
+    
+    punc = string.punctuation
+    for c in punc:
+        if c in text:
+            text = text.replace(c, ' ')
+    
+    tokens = nltk.word_tokenize(text)
+#     print(tokens)
+    res = []
+    
+    for token in tokens:
+        try:
+            word = lemmatizer.lemmatize(token)
+            if len(word)>1:
+                try:
+                    int(word)
+                except:
+                    res.append(str(word))
+        except:
+            continue
+    docs=nltk.word_tokenize(" ".join(res))
+    return res
+
+def get_rare_words(tokens_ls):
+    """ use the word count information across all tweets in training data to come up with a feature list
+    Inputs:
+        processed_tweets: pd.DataFrame: the output of process_all() function
+    Outputs:
+        list(str): list of rare words, sorted alphabetically.
+    """
+    counter = Counter([])
+    for tokens in tokens_ls:
+        counter.update(tokens)
+    
+    rare_tokes = [k for k,v in counter.items() if v==1]
+    rare_tokes.sort()
+    return rare_tokes
+
+
+def process(lyrics, batchSize=50):
+	"""
+	It will change lyrics to vetors as well as build the
+	features and labels for LSTM
+
+	lyric: list of str. all of the lyrics
+	return: (X, Y, vocab_size, vocab_ID, vocab)
+	"""
+
+    lyricDocs = seperate(lyrics)
+    print("Totally %d lyrics."%len(lyricDocs))
+
+    allWords = {}
+    for lyricDoc in lyricDocs:
+        for word in lyricDoc:
+            if word not in allWords:
+                allWords[word] = 1
+            else:
+                allWords[word] += 1
+
+    wordPairs = sorted(allWords.items(), key = lambda x: -x[1])
+    words, a= zip(*wordPairs)
+    #print(words)
+    words += (" ", )
+    wordToID = dict(zip(words, range(len(words)))) #word to ID
+    wordTOIDFun = lambda A: wordToID.get(A, len(words))
+
+    lyricVector = [([wordTOIDFun(word) for word in lyricDoc]) for lyricDoc in lyricDocs] 
+
+	batchNum = (len(lyrics) - 1) // batchSize + 1
+
+	X = []
+	Y = []
+
+	for i in range(batchNum):
+		batchVec = lyricVector[i*batchSize: (i+1)*batchSize]
+
+		maxLen = max([len(vector) for vector in batchVec])
+
+		temp = np.full((batchSize, maxLen), ,np.int32)
+
+		for j in range(batchSize):
+			temp[j, :len(batchVec[j])] = batchVec[j]
+
+		X.append(temp)
+
+		temp_copy = np.copy(temp)
+		temp_copy[:, :-1] = temp[:, 1:]
+
+		Y.append(temp2)
+
+	return X, Y, len(words) + 1, wordToID, words
