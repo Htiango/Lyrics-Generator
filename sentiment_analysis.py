30cd2242ac5773195b2f24bf94642a5ea2691609
Htiango <hongty106@gmail.com>
2018-04-14 15:46:49 -0400

    parse the json result

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -7,6 +7,10 @@ This script is a demo of how to get sentiment analysis from an online API.
 Remember each IP can only request 1000 times per day.
 If you want to exceed the limitation, you might need to automatic change 
 the IP in case of failure.
+
+
+Usage: Run "python3 sentiment_analysis -t [TEXT YOU WANT TO ANALYSIS]",
+       and you will get the result print out on the console.
 """
 
 URL = "http://text-processing.com/api/sentiment/"
@@ -16,8 +20,13 @@ def get_sentiment(args):
 	text = args.text
 	param = prefix + text
 	response = requests.post(URL, param)
-	res = json.dumps(response.json())
-	print(res)
+	res = response.json()
+	pos_dict = res['probability']
+	label = res['label']
+	print("The label is: " + label)
+	print("The positive possibility is: %f"%pos_dict["pos"])
+	print("The negative possibility is: %f"%pos_dict["neg"])
+	print("The netural possibility is: %f"%pos_dict["neutral"])
 
 def main():
     parser = argparse.ArgumentParser()
