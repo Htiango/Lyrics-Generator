1b872a50ed8d917369eb1f27f0abe6f881e356f5
Htiango <hongty106@gmail.com>
2018-04-29 16:24:54 -0400

    only accept loading parameter files

--- codes/main.py
+++ codes/main.py
@@ -5,21 +5,23 @@ import argparse
 # import lyric_process_old
 import pickle
 
-param_saving_path = "../data/param.dat"
+# param_saving_path = "../data/param.dat"
 
 def run(args):
+
+    param_saving_path = args.filename
     
     if args.mode == "train":
-        X, Y, wordNum, wordToID, words = lyric_processing.pretreatment(args.filename)
-        data = {'X': X, "Y":Y, "wordNum":wordNum, "wordToID": wordToID, "words":words}
+        # X, Y, wordNum, wordToID, words = lyric_processing.pretreatment(args.filename)
+        # data = {'X': X, "Y":Y, "wordNum":wordNum, "wordToID": wordToID, "words":words}
 
-        with open(param_saving_path, 'wb') as f:
-            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-            
-        print("model saved!")
+        # with open(param_saving_path, 'wb') as f:
+        #     pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
-        # with open(param_saving_path, 'rb') as f:
-        #     data = pickle.load(f)
+        # print("model saved!")
+
+        with open(param_saving_path, 'rb') as f:
+            data = pickle.load(f)
 
         print("training...")
         model.train(data['X'], data['Y'], data['wordNum'])
@@ -34,7 +36,7 @@ def run(args):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename', type=str, 
-        required=True, help='input the path of the csv file of the lyric.')
+        required=True, help='input the path of the param data.')
     parser.add_argument("-m", "--mode", help = "select mode by 'train' or test",
         choices = ["train", "test"], default = "test")
     
