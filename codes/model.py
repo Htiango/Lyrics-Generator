50a49ce3b9f2f76764f7c98f026e9342b1b9610d
Htiango <hongty106@gmail.com>
2018-04-29 16:29:31 -0400

    fix the checkpoint path

--- codes/model.py
+++ codes/model.py
@@ -73,9 +73,9 @@ def train(X, Y, wordNum, reload=True):
                 print("epoch: %d steps:%d/%d loss:%3f" % (epoch,step,epochSteps,loss))
                 if globalStep%1000==0:
                     print("save model")
-                    save_path = saver.save(sess, '/output/model.ckpt')
-                    # print("Model saved in file: %s" % save_path)
-                    saver.save(sess,checkpointsPath + "/lyric",global_step=epoch)
+                    # save_path = saver.save(sess, '/output/model.ckpt')
+                    save_path = saver.save(sess,checkpointsPath + "/lyric",global_step=epoch)
+                    print("Model saved in file: %s" % save_path)
 
 def probsToWord(weights, words):
     """probs to word"""
